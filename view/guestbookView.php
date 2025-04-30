f414e5488719e58b8f8e7e1e473708c1ca46dda8
NikGorban <masarash12345@gmail.com>
2025-04-30 13:16:35 +0200

    il_y_a_X_message-OK

--- view/guestbookView.php
+++ view/guestbookView.php
@@ -16,55 +16,50 @@
 <h1>TI2 | Livre d'or</h1>
 <!-- Formulaire d'ajout d'un message -->
 <div class="container">
-<h2>Ici le formulaire</h2>
+    <h2>Ici le formulaire</h2>
 
-<?php if (isset($erreur)):?>
-    <h4 class="err"><?=$erreur?></h4>
-<?php endif; ?>
+    <?php if (isset($erreur)):?>
+        <h4 class="err"><?=$erreur?></h4>
+    <?php endif; ?>
 
-<img src="img/sign-up-amico.png" height="400px" alt="">
-<div class="formulaire">
+    <img src="img/sign-up-amico.png" height="400px" alt="">
+    <div class="formulaire">
 
-    <form action="" method="post">
-        <div class="bloc">
-            <label for="firstname">Prenom *</label>
-        <input type="text" name="firstname" id="firstname" maxlength="100" required>
-        </div>
-        <div class="bloc">
-            <label for="lastname">Nom *</label>    
-        <input type="text" name="lastname" id="lastname" maxlength="100" required>
-        </div>
-        <div class="bloc">
-             <label for="usermail">Email *</label>
-        <input type="email" name="usermail" id="usermail" maxlength="200" required>
-        </div>
-        <div class="bloc">
-            <label for="postcode">c /postal *</label>
-        <input type="text" name="postcode" id="postcode" maxlength="4" required>  
-        </div>
-        <div class="bloc">
-            <label for="phone">Portable *</label>
-        <input type="tel" name="phone" id="phone" maxlength="20" required>
-        </div>
-        <div class="bloc">
-            <label for="message">Message *</label>
-        <textarea name="message" id="message" rows="6" maxlength="300" required style = "resize: none;"></textarea>
-        </div> 
+        <form action="" method="post">
+            <div class="bloc">
+                <label for="firstname">Prenom *</label>
+                <input type="text" name="firstname" id="firstname" maxlength="100" required>
+            </div>
 
-        <button type="submit">Envoyer</button>
-    </form>
+            <div class="bloc">
+                <label for="lastname">Nom *</label>    
+                <input type="text" name="lastname" id="lastname" maxlength="100" required>
+            </div>
+
+            <div class="bloc">
+                 <label for="usermail">Email *</label>
+                <input type="email" name="usermail" id="usermail" maxlength="200" required>
+            </div>
+
+            <div class="bloc">
+                <label for="postcode">c /postal *</label>
+                <input type="text" name="postcode" id="postcode" maxlength="4" required>  
+            </div>
+
+            <div class="bloc">
+                <label for="phone">Portable *</label>
+                <input type="tel" name="phone" id="phone" maxlength="20" required>
+            </div>
+
+            <div class="bloc">
+                <label for="message">Message *</label>
+                <textarea name="message" id="message" rows="6" maxlength="300" required style = "resize: none;"></textarea>
+            </div> 
+
+            <button type="submit">Envoyer</button>
+        </form>
+    </div>
 </div>
-</div>
-
-
-
-<ul>
-    <li>
-        <p><strong>firstname lastname</strong></p>
-        <p><em>datemessage</em></p>
-        <p>message</p>
-    </li>
-
 
 <?php
 // si on a pas de message $nbMessage === 0
@@ -83,16 +78,21 @@ else:
 <div class="messages">
     <h2>Il y a <?=$nbMessage?> message<?=$pluriel?></h2>
     
-    <?php
-    // ici affichage de la pagination
-    // tant qu'on a des messages
-
+<?php
     foreach ($getmessage as $message):
+?>
 
-    ?>
-    <h4>Ecrit par <?=$message['firstname']?> le <?=dateFR($message['datemessage'])?></h4>
-    <p><?=nl2br($message['message'])?></p>
-    <hr>
+    <ul>
+        <li>
+            <p><strong>
+            <?=$message['firstname']." ".$message['lastname'] ?>
+            </strong></p>
+            <p><em>
+            <?=dateFR($message['datemessage'])?>
+            </em></p>
+            <p><?=nl2br($message['message'])?></p>    
+        </li>
+    </ul>
     <?php
     endforeach;
 endif;
@@ -102,12 +102,12 @@ endif;
 <!-- Pagination (BONUS) -->
 <?php
 // À commenter quand on a fini de tester
-echo "<h3>Nos var_dump() pour le débugage</h3>";
-echo '<p>$_POST</p>';
-var_dump($_POST);
-echo '<p>$_GET</p>';
-var_dump($_GET);
-// var_dump($insert);
+// echo "<h3>Nos var_dump() pour le débugage</h3>";
+// echo '<p>$_POST</p>';
+// var_dump($_POST);
+// echo '<p>$_GET</p>';
+// var_dump($_GET);
+
 ?>
 
 <script src="js/validation.js"></script>
