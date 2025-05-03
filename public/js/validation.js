2f4c1e47c420414f208cfb80d327270913d0eaaf
Sola Kabuta <solakabuta@gmail.com>
2025-05-03 23:35:59 +0200

    fix: fixed js file mergin conflict

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-console.log(`it's working`)
-
-let firstName = document.getElementById('prenom').value
-let lastName = document.getElementById('nom').value
-let mail = document.getElementById('email').value;
-let postal = document.getElementById('postal').value;
-let phone = document.getElementById('portable').value;
-let message = document.getElementById('message').value;
-
-const form = document.getElementById('form')
-
-const allInput = [firstName, lastName, mail, postal, phone, message]
-
-
-// REGEX
-
-function verifyMail(email) {
-    const mailRegex = /^([\w]+)@([\w]+).(\w{2,3})$/
-    return mailRegex.test(email)
-}
-
-function verifyPostal(postal) {
-    const postalRegex = /^\d{4}$/
-    return postalRegex.test(postal)
-}
-
-function verifyPhone (phone) {
-    const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
-    return phoneRegex.test(phone)
-}
-
-// MESSAGES
-
-let firstNameError = document.getElementById('firstname-error')
-let lastNameError = document.getElementById('lastname-error')
-let mailError = document.getElementById('mail-error')
-let postalError = document.getElementById('postal-error')
-let phoneError= document.getElementById('phone-error')
-let messageError= document.getElementById('message-error')
-
-let sucessMessage = document.getElementById('success-message')
-
-const allErrors = [firstNameError, lastNameError, mailError, postalError, phoneError, messageError]
-
-
-// EVENTS
-
-form.addEventListener("submit", function(e){
-    e.preventDefault()
-    const email = mail.trim()
-    const postalCode = postal.trim()
-    const phoneNumber = phone.trim()
-
-    if (verifyMail(email) && verifyPostal(postalCode) && verifyPhone(phoneNumber)) {
-        sucessMessage.style.display = "block"
-    } else if (firstName === "") {
-        firstNameError.style.display = "block"
-    } else if (lastName === "") {
-        lastNameError.style.display = "block"
-    }
-    
-    
-    
-//     for (let i = 0; allInput.length; i++) {
-//         for (let y = 0; allErrors.length; y++) {
-//             if (allInput[i] === " ") {
-//                 allErrors[y].style.display = "block";
-//             } else if (allInput != " ") {
-//                 sucessMessage.style.display = "block"
-//             }
-//         }
-//     } 
-    
-})
-=======
 console.log("It's working ! ")
 
 const form = document.getElementById("form")
@@ -167,4 +91,4 @@ form.addEventListener("submit", function(e) {
 // setCounter()
 
 
->>>>>>> main
+
--- view/guestbookView.php
+++ view/guestbookView.php
@@ -7,218 +7,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TI2 | Livre d'or</title>
-    <link rel="icon" type="image/png" href="../public/img/favicon.png">
-    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="icon" type="image/png" href="img/favicon.png">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-<<<<<<< HEAD
-    <section class="title">
-        <h1>TI2 | Livre d'or</h1>
-        <!-- Formulaire d'ajout d'un message -->
-        <!-- This one is gonna be hidden on desktop -->
-        <h2 class="subtitle">Laissez-nous un message</h2>
-       
-        <?php
-// si on a inséré un article
-if(isset($thanks)):
-    ?>
-    <h3 class="thanks"><?=$thanks?></h3>
-<?php
 
-elseif(isset($error)):
-    ?>
-    <h4 class="error"><?=$error?></h4>
-<?php
-endif;
-?>
-    </section>
-    <section class="form-container">
-        <img class="banner" src="../public/img/sign-up-amico.png" alt="form-img" width="500" height="500">
-        <div>
-            <h2 class="second-sub">Laissez-nous un message</h2>
-        <p id="firstname-error" class="error-message">Prénom ne peut pas être vide</p>
-        <p id="lastname-error" class="error-message">Nom ne peut pas petre vide</p>
-        <p id="mail-error" class="error-message">Adresse email invalide</p>
-        <p id="postal-error" class="error-message">Code postal email invalide</p>
-        <p id="phone-error" class="error-message">Numéro de portable invalide</p>
-        <p id="message-error" class="error-message">Message invalide</p>
-        <p id="success-message" class="success-message">Toutes les informations sont valides</p>
-        <form id="form" action="" method="post">
-            <div class="fieldset">
-                <label id="prenom-label" for="prenom">Prénom *</label>
-                <input type="text" name="prenom" id="prenom">
-            </div> <br>
-            <div class="fieldset">
-                <label for="nom">Nom *</label>
-                <input type="nom" name="nom" id="nom">
-            </div> <br>
-            <div class="fieldset">
-                <label for="email">Email *</label>
-                <input name="email" id="email" rows="10"></input>
-            </div> <br>
-            <div class="fieldset">
-                <label for="">c/ Postal *</label>
-                <input type="text" id="postal" name="postal">
-            </div> <br>
-            <div class="fieldset">
-                <label for="portable">Portable *</label>
-                <input type="text" name="portable" id="portable">
-            </div> <br>
-            <div class="message-field">
-                <label for="message">Message *</label>
-                <textarea name="message" id="message"></textarea> <br>
-            </div>
-            <button id="btn" type="submit">Envoyer</button>
-        </form>
-        </div>
-    </section>
-    <section>
-        <!-- Si pas de message -->
-        <h3>Pas encore de message</h3>
-    </section>
+<div>
     <?php
-    // articles est un tableau vide
-    if (empty($guestBook)):
+    $error = "";
+    $thanks = "";
+    if (isset($insert)) {
+        if ($insert === true) {
+            $thanks = "Message bien envoyé";
+        } elseif ($insert === false) {
+            $error = " Pas inséré côté serveur";
+        }
+    }
+
     ?>
-        <div class="nomessage">
-            <h3>Pas encore d'article</h3>
-        <?php
-    // nous avons au moins un article
-    else:
-        // on peut compter le nombre d'articles
-        $countGuestBook = count($guestBook);
-        // ternaire pour ajouter un s à article
-        // si on en a plus d'un
-        $pluriel = $countGuestBook > 1 ? "s" : "";
-        ?>
-            <div class="messages">
-                <h3>Nous avons <?= $countGuestBook ?> guestbook<?= $pluriel ?></h3>
-                <hr>
-
-                <?php
-                // tant qu'on a des articles
-                foreach ($guestBook as $gb):
-                ?>
-                    <h4><?= $gb['firstname'] ?></h4>
-                    <p><?= nl2br($gb['lastname']); // retour à la ligne automatique
-                        ?></p>
-                    <h5><?= $gb['text'] ?></h5>
-                    <hr>
-            <?php
-                endforeach;
-            endif;
-            ?>
-            </div>
-            <section class="msg-container">
-                <?php
-                #var_dump($_POST,$articles,$countArticle);
-                ?>
-                <!-- Si 1 message -->
-                <h3>Il y a 1 message</h3>
-                <!-- Si plusieurs messages -->
-                <h3>Il y a X messages</h3>
-            </section>
+</div>
+<section>
+    <h1>TI2 | Livre d'or</h1>
+    <p id="success"></p>
+    <p id="error"> </p>
+    <form id="form" action="" method="post">
+        <div class="inputs-container">
+            <label for="lastname">Prénom</label> <br>
+            <input type="text" name="firstname" id="firstname" placeholder="Jane"> <br>
+            <label for="firstname">Nom</label> <br>
+            <input type="text" name="lastname" id="lastname" placeholder="Doe"> <br>
+            <label for="usermail">Email</label> <br>
+            <input type="text" name="usermail" id="usermail" placeholder="janedoe@gmail.com"> <br>
+            <label for="postcode">Postcode</label> <br>
+            <input type="text" name="postcode" id="postcode" placeholder="1000"> <br>
+            <label for="phone">Téléphone</label> <br>
+            <input type="text" name="phone" id="phone" placeholder="0473 02 88 37 52"> <br>
+            <div class="msg-container">
+                <label for="message">Message</label> <br>
+                <textarea name="message" id="message" maxlength="300"></textarea>
+                <p id="char-counter"><span>Tu as atteint la limite de caractères ! </span> / 300</p>
+            </div> <br>
+            <button type="submit">Envoyer</button>
+        </div>
+        <img src="../public/img/GpmkQB_XcAA09uf.jpeg" alt="form-img" width="500" height="800">
+    </form>
+</section>
 
 
+<div id="post-container">
 
-            <!-- Pagination (BONUS) 
-
-            <!-- Liste des messages -->
-            <!-- <section>
-                <ul>
-                    <li>
-                        <p><strong>firstname lastname</strong></p>
-                        <p><em>datemessage</em></p>
-                        <p>message</p>
-                    </li>
-                    <!-- Autres messages -->
-                    <!-- <li>
-                        <p><strong>firstname lastname</strong></p>
-                        <p><em>datemessage</em></p>
-                        <p>message</p>
-                    </li>
-                </ul> -->
-            <!-- </section>  -->
-            <!-- etc ... -->
-
-
-
-
-
-
-            <!-- Pagination (BONUS)
+    <!-- Pagination (BONUS) -->
     <?php
     // À commenter quand on a fini de tester
     // echo "<h3>Nos var_dump() pour le débugage</h3>";
     // echo '<p>$_POST</p>';
-    // var_dump($_POST);
-    // echo '<p>$_GET</p>';
-    // var_dump($_GET);
-    // // ?> -->
+    echo $_POST['firstname'] . ' à écrit : ';
+    echo ($_POST['message']);
+    ?>
+</div>
 
-            <script src="../public/js/validation.js" defer></script>
-=======
-
-    <div>
-        <?php
-        $error = "";
-        $thanks = "";
-        if (isset($insert)) {
-            if ($insert === true) {
-                $thanks = "Message bien envoyé";
-            } elseif ($insert === false) {
-                $error = " Pas inséré côté serveur";
-            }
-        }
-
-        ?>
-    </div>
-    <section>
-        <h1>TI2 | Livre d'or</h1>
-        <p id="success"></p>
-        <p id="error"> </p>
-        <form id="form" action="" method="post">
-            <div class="inputs-container">
-                <label for="lastname">Prénom</label> <br>
-                <input type="text" name="firstname" id="firstname" placeholder="Jane"> <br>
-                <label for="firstname">Nom</label> <br>
-                <input type="text" name="lastname" id="lastname" placeholder="Doe"> <br>
-                <label for="usermail">Email</label> <br>
-                <input type="text" name="usermail" id="usermail" placeholder="janedoe@gmail.com"> <br>
-                <label for="postcode">Postcode</label> <br>
-                <input type="text" name="postcode" id="postcode" placeholder="1000"> <br>
-                <label for="phone">Téléphone</label> <br>
-                <input type="text" name="phone" id="phone" placeholder="0473 02 88 37 52"> <br>
-                <div class="msg-container">
-                    <label for="message">Message</label> <br>
-                    <textarea name="message" id="message" maxlength="300"></textarea>
-                    <p id="char-counter"><span>Tu as atteint la limite de caractères ! </span> / 300</p>
-                </div> <br>
-                <button type="submit">Envoyer</button>
-            </div>
-            <img src="../public/img/GpmkQB_XcAA09uf.jpeg" alt="form-img" width="500" height="800">
-        </form>
-    </section>
-
-
-    <div id="post-container">
-       
-        <!-- Pagination (BONUS) -->
-        <?php
-        // À commenter quand on a fini de tester
-        // echo "<h3>Nos var_dump() pour le débugage</h3>";
-        // echo '<p>$_POST</p>';
-        echo $_POST['firstname'] . ' à écrit : ';
-        echo ($_POST['message']);
-        ?>
-    </div>
-
-    <script src="../public//js/validation.js"></script>
->>>>>>> main
+<script src="../public//js/validation.js"></script>
 </body>
 
 </html>
